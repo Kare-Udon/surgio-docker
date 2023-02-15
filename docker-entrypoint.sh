b3e9ee74c85107fe1554d55be9ceb3c8912e3a71
Kare-Udon <gpg@udon.eu.org>
2023-02-15 13:01:44 +0800

    Update: docker entrypoint add cron job for auto generate config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,21 @@ if [ -f /surgio/package.json ];then
     sed -i '/"scripts": {/a "generage": "surgio generate",' /surgio/package.json
 fi
 
+# add cron job
+if [ $AUTOGEN_INTERVAL ];then
+    hour=$(echo "$AUTOGEN_INTERVAL" | grep -oP '\d+(?=h)')
+    minute=$(echo "$AUTOGEN_INTERVAL" | grep -oP '\d+(?=m)')
+    if [[ -n $hour ]] && [[ -n $minute ]]; then
+        cron_time="*/$minute */$hour * * *"
+    elif [[ -n $hour ]]; then
+        cron_time="0 */$hour * * *"
+    elif [[ -n $minute ]]; then
+        cron_time="*/$minute * * * *"
+    fi
+    echo "$cron_time cd /surgio && npx surgio generate" >> /etc/crontabs/root
+    crond
+fi
+
 # generate rules and run gateway
 cd /surgio
 npm run generage && node gateway.js
